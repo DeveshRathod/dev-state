dd009eb3ac92c62ade40ddec6b8e3029d56ddb3a
Devesh Rathod <108536407+DeveshRathod@users.noreply.github.com>
2023-12-31 16:04:32 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <h1 className=" text-red-500">Devesh</h1>;
+  return <h1 className=" text-red-500">Rathod</h1>;
 };
 
 export default App;
